2aac364af63600ac74c5ec04d27d0804b30899e7
h-wata <hirofumi01.watanabe@g.softbank.co.jp>
2024-06-13 18:57:17 +0900

    Add autopep8 and flake8 in none-ls-extras.nvim

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,6 +3,7 @@
 ---@type LazySpec
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = { "nvimtools/none-ls-extras.nvim" },
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
     local null_ls = require "null-ls"
@@ -11,15 +12,14 @@ return {
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
+      require "none-ls.diagnostics.flake8",
+      require "none-ls.formatting.autopep8",
       -- Set a diagnostic
-      null_ls.builtins.diagnostics.pylint,
-      null_ls.builtins.diagnostics.tidy,
       null_ls.builtins.diagnostics.markdownlint,
       -- Set a formatter
       null_ls.builtins.formatting.stylua,
       null_ls.builtins.formatting.prettier,
       null_ls.builtins.formatting.isort,
-      null_ls.builtins.formatting.xmllint,
     }
     return config -- return final config table
   end,
