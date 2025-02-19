5badb3e2667245687ca5bb3aa4ca8cde93af0dc2
h-wata <hirofumi01.watanabe@g.softbank.co.jp>
2025-02-19 15:35:47 +0900

    refactor(none-ls): update ruff and yapf configurations with extra_args

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,19 +7,24 @@ return {
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
     local null_ls = require "null-ls"
+    local home = os.getenv "HOME"
 
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
-      require "none-ls.diagnostics.ruff",
+      require("none-ls.diagnostics.ruff").with {
+        extra_args = { "--config=" .. home .. "/work/.github/pyproject.toml" },
+      },
       -- require "none-ls.formatting.ruff",
       -- Set a diagnostic
       null_ls.builtins.diagnostics.markdownlint,
       null_ls.builtins.diagnostics.cmake_lint,
       -- Set a formatter
       null_ls.builtins.formatting.remark,
-      null_ls.builtins.formatting.yapf,
+      null_ls.builtins.formatting.yapf.with {
+        extra_args = { "--style=" .. home .. "/work/.github/pyproject.toml" },
+      },
       null_ls.builtins.formatting.stylua,
       null_ls.builtins.formatting.prettier,
       null_ls.builtins.formatting.cmake_format,
